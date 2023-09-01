946dcde60f1dc9fb17b6352aca1039c2dc0603cb
MrYousri97 <mahmoudyousri2011@gmail.com>
2023-09-01 20:15:27 +0300

    Done

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
       <h2>Projects</h2>
       <div className="container projects__container">
         {
-          data.map(({id,image,title,gituhub})=>{
+          data.map(({id,image,title,github})=>{
             return(
               <article key={id} className='project__item'>
                   <div className="project__item-img">
@@ -15,7 +15,7 @@ const Projects = () => {
                   </div>
                   <h3>{title}</h3>
                   <div className="project__item-cta">
-                  <a href={gituhub} className='btn'>Github</a>
+                  <a href={github} className='btn' target='_blank' >Github</a>
                   </div>
               </article>
             )   
